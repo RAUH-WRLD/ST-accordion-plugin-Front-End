e9f2f2227062897e1deac9fa04089ba0d942f326
tweltixx <68005798+RAUH-WRLD@users.noreply.github.com>
2020-12-27 23:00:59 +0300

    accordion-plugin
    accordion

---
+++ source/index.js
@@ -0,0 +1,6 @@
+import "../source/style.scss";
+import {stAccordion} from "../source/ST-accordion-plugin/ST-accordion";
+stAccordion({
+    eachAccordionItem: ".accordion-list__item",
+    eachAccordionInfo: ".accordion__info"
+});
